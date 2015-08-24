4c4377aa40d51e4e8c5c67dc1e798dc69524c679
Your Actual Name <iannapier02@gmail.com>
2015-08-24 21:48:08 +0100

    added change to task customer acton

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,16 @@ class TasksController < ApplicationController
     end
   end
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
+  
+  def change
+
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html { redirect_to tasks_path, notice: "Task Update"}
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
--- db/migrate/20150822124026_add_user_id_to_tasks.rb
+++ db/migrate/20150822124026_add_user_id_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddUserIdToTasks < ActiveRecord::Migration
+ class AddUserIdToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :user_id, :integer
   end
